950f5c4444caa1c433580e49f2a210aadd210208
dave4283 <dave4283@gmail.com>
2023-09-01 13:08:28 -0500

    full stack push #2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import { Container } from "react-bootstrap";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import CartScreen from "./screens/CartScreen";
  
 function App() {
   return (
@@ -16,6 +18,7 @@ function App() {
             {" "}
             <Route path="/" element={<HomeScreen />} />
             <Route path="/product/:id" element={<ProductScreen />} />
+            <Route path="/cart/:id?" element={<CartScreen />} />
           </Routes>
         </Container>
       </main>
